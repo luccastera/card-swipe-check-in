0cae6883fec55ac5e394ff8155b135e76c00d153
Luc Castera <luc.castera@gmail.com>
2016-07-30 12:39:28 -0400

    wired up frontend

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//var Keyboard = require('node-keyboard');
 var express    = require('express');
 var SerialPort = require("serialport");
 var port = new SerialPort('/dev/input/event0');
@@ -11,13 +10,19 @@ var makers = [];
 
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, res) {
-  res.send('Card Swipe Reader');
+  res.render('pages/index');
 });
 
 app.get('/makers.json', function(req, res) {
   return res.json(makers);
 });
+
+app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
