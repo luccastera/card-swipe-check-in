735a3a72376a28f1e5fd3e5b85228d62c9bd967c
Luc Castera <luc.castera@gmail.com>
2016-07-30 14:21:44 -0400

    wired up ajax

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  function getMakers() {
+    console.log('ajax');
+    $.ajax({
+      method: 'GET',
+      dataType: 'json',
+      url: '/makers.json',
+      success: function(makers) {
+        if (Array.isArray(makers)) {
+          if (makers.length === 0) {
+            $('#makers').html('<p class="text-muted">No one has checked in.</p>');
+          } else {
+            $('#makers').html('<ul></ul>');
+            makers.forEach(function(maker) {
+              $('#makers ul').append('<li>' + maker + '</li>');
+            });
+          }
+        }
+      }
+    });
+  }
+  window.setInterval(getMakers, 1000);
+});
