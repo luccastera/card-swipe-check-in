591267b4c14252668831527b2e3f99b0014bfaad
Luc Castera <luc.castera@gmail.com>
2016-07-30 13:12:12 -0400

    try keyboard.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var express    = require('express');
 var StringDecoder = require('string_decoder').StringDecoder;
-var SerialPort = require("serialport");
-var port = new SerialPort('/dev/input/event0', {
-});
+//var SerialPort = require("serialport");
+//var port = new SerialPort('/dev/input/event0', {
+//});
 
-port.on('data', function(data) {
-  console.log(data);
-  var decoder = new StringDecoder('hex');
+//port.on('data', function(data) {
   //console.log(data);
-  //var buff = new Buffer(data);
-  //var str = data.toString('hex');
-  decoder.write(data);
-  var str = decoder.end();
-  console.log(str);
-});
+  //var decoder = new StringDecoder('hex');
+  ////console.log(data);
+  ////var buff = new Buffer(data);
+  ////var str = data.toString('hex');
+  //decoder.write(data);
+  //var str = decoder.end();
+  //console.log(str);
+//});
+
+var Keyboard = require('./keyboard.js');
+var k = new Keyboard('event0'); // 'event2' is the file corresponding to my keyboard in /dev/input/
+k.on('keyup', console.log);
+k.on('keydown', console.log);
+k.on('keypress', console.log);
+k.on('error', console.error);
 
 var makers = [];
 
